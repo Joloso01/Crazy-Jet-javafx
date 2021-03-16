89fbf003cf4162ccc16e4c79cb15c8fe065774da
jose <jbermejop01@elpuig.xeill.net>
2021-03-16 17:34:58 +0100

    mainMenu hecho

--- src/main/java/controller/MainWindow.java
+++ src/main/java/controller/MainWindow.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class MainWindow {
+    Stage stage;
+    Scene scene;
+
+    public void setStage(Stage primaryStage) {
+        stage = primaryStage;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
 }
