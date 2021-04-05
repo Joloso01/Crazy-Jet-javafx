f286c1525fadce3563c03516fa116703173f683b
fernando <fcano@elpuig.xeill.net>
2021-04-05 19:03:46 +0200

    mapa añadido

---
+++ target/classes/model/EnemyJet.java
@@ -0,0 +1,10 @@
+package model;
+
+import javafx.scene.image.Image;
+
+public class EnemyJet extends Sprite{
+
+    public EnemyJet(Image image) {
+        super(image);
+    }
+}
---
+++ target/classes/model/Jet.java
@@ -0,0 +1,29 @@
+package model;
+
+import javafx.scene.image.Image;
+
+public class Jet extends Sprite {
+
+    public Jet(Image image) {
+        super(image);
+    }
+
+    @Override
+    public void move(String direction) {
+        switch (direction) {
+            case "RIGHT":
+                setX(getPosX() + 4);
+                break;
+            case "LEFT":
+                setX(getPosX() - 4);
+                break;
+            case "UP":
+                setY(getPosY()-4);
+                break;
+            case "DOWN":
+                setY(getPosY()+4);
+                break;
+        }
+    }
+
+}
