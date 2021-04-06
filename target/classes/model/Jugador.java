a58d4207af3698c5262fb3f888e7c80d98abb79e
Joloso01 <jbermejop01@elpuig.xeill.net>
2021-04-06 21:58:52 +0200

    estadisticas revisar

---
+++ target/classes/model/Jugador.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Jugador {
+    private String nom;
+    private int puntuacion=0;
+    private int tiempo=0;
+    private String fecha;
+
+    public Jugador(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
