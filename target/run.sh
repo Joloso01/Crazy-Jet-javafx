b437be6fa5fbb330a02ffa22f19b2c9d0a8696f1
jose <jbermejop01@elpuig.xeill.net>
2021-04-16 18:27:58 +0200

    acabado

---
+++ target/run.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+java --module-path ../../../Escriptori/openjfx-15.0.1_linux-x64_bin-sdk/javafx-sdk-15.0.1//lib --add-modules javafx.controls,javafx.fxml,javafx.media -jar joc_javaFX-1.0-SNAPSHOT-jar-with-dependencies.jar
